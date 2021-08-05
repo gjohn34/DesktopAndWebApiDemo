2641c3db6fafa5f6c0d3271b83f55f495a072175
Glen Johnson <glenjohnson39@yahoo.com.au>
2021-08-05 14:05:10 +1000

    Connecting LoginView w/ light val

--- RetailManager/RetailDesktop/Bootstrapper.cs
+++ RetailManager/RetailDesktop/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RetailDesktop.Helpers;
 using RetailDesktop.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RetailDesktop
 {
@@ -15,6 +17,11 @@ namespace RetailDesktop
         public Bootstrapper()
         {
             Initialize();
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+            PasswordBoxHelper.BoundPasswordProperty,
+            "Password",
+            "PasswordChanged");
         }
 
         protected override void Configure()
---
+++ RetailManager/RetailDesktop/ViewModels/LoginViewModel.cs
@@ -0,0 +1,61 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RetailDesktop.ViewModels
+{
+    public class LoginViewModel : PropertyChangedBase
+    {
+        string _userName;
+        string _password;
+
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+
+            }
+        }
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+
+        public bool CanLogIn
+        {
+            get
+            {
+                bool output = false;
+                if (!string.IsNullOrWhiteSpace(Password) && !string.IsNullOrWhiteSpace(Password))
+                {
+                    output = true;
+                }
+
+                return output;
+
+            }
+
+            // Why doesn't this work?
+        }
+
+        public void LogIn()
+        {
+                bool t = true;
+        }
+    }
+}
